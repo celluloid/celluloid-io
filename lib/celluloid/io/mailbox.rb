64081f188cf88cb83f4a3da538d8c7911dffd96e
//de <de@freed.network>
2015-04-13 17:05:13 -0700

    use Celluloid::Mailbox::Evented convention

--- lib/celluloid/io/mailbox.rb
+++ lib/celluloid/io/mailbox.rb
@@ -1,7 +1,7 @@
 module Celluloid
   module IO
     # An alternative implementation of Celluloid::Mailbox using Reactor
-    class Mailbox < Celluloid::EventedMailbox
+    class Mailbox < Celluloid::Mailbox::Evented
       def initialize
         super(Reactor)
       end
